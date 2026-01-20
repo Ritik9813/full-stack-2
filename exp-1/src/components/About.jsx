55b45565734ef5903b4c5c4bf04238d320b4d9fa
ritik kumar singh <ritikkumarsingh@ritiks-MacBook-Air.local>
2026-01-20 14:51:45 +0530

    exp1

---
+++ exp-1/src/components/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>My name is Ritik kumar singh</p>
+      <p>my uid is 23BIA50035</p>
+    </div>
+  );
+}
+
+export default About;
