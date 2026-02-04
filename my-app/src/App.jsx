6a914cd92371c02b66a6054584af680152bcbe61
ritik kumar singh <ritikkumarsingh@ritiks-MacBook-Air-2.local>
2026-02-04 11:36:28 +0530

    exp2

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import ButtonExample from './components/Button';
+import DropdownExample from './components/Dropdowns';
+
+function App() {
+  return (
+    <>
+      <ButtonExample />
+      <DropdownExample />
+    </>
+  );
+}
+
+export default App;
