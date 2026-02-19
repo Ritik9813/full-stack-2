c5a5eb1d260629c5ca8d94e298d04c7798a050d8
ritik kumar singh <ritikkumarsingh@ritiks-MacBook-Air-2.local>
2026-02-20 00:03:58 +0530

    exp-r

---
+++ exp-4/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: "20px", textAlign: "center" }}>
+      © 2025 Ritik Kumar Singh. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
