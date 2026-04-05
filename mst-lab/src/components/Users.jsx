b9f463e9413085d42c9571e7417ee9d8a2286bcc
ritik kumar singh <ritikkumarsingh@ritiks-MacBook-Air-2.local>
2026-04-05 16:23:59 +0530

    exp-7

---
+++ mst-lab/src/components/Users.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+function Users() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <b>{user.name}</b> - {user.email}
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}
+
+export default Users;
