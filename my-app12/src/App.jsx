d06b2244080ac35ca808f8736cc52a3b6238a29e
ritik kumar singh <ritikkumarsingh@ritiks-MacBook-Air-2.local>
2026-04-17 22:09:22 +0530

    exp-8

---
+++ my-app12/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button'
+
+function App() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Vite + React Working 🚀</h1>
+      <Button variant="primary">React Bootstrap OK</Button>
+    </div>
+  )
+}
+
+export default App
